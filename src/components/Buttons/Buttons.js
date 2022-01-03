8c8905c0c9790beb9066b5e7c3579fdc5d3b9007
Muniyaraj <mnraj218@gmail.com>
2022-01-03 09:28:30 +0530

    init setup

---
+++ src/components/Buttons/Buttons.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+function Buttons() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Button variant='contained'>Test Button</Button>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Buttons;
